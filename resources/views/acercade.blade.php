94ab502c1dd5d7c7bf1ce9d791f703f82f288464
jhon06 <www-jrhuarsocca@gmail.com>
2023-07-23 16:14:53 -0500

    Primer commit - Proyecto FinalPMN

---
+++ resources/views/acercade.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Acerca de</title>
+</head>
+<body>
+    <h1>Información personal:</h1>
+    <p>Apellidos y Nombres: [Tu nombre completo]</p>
+    <p>Carrera Profesional: [Tu carrera profesional]</p>
+    <p>Nivel/Ciclo de estudios: [Tu nivel/ciclo de estudios]</p>
+    <p>Turno: [Tu turno]</p>
+</body>
+</html>
